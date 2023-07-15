822f8fca486ce30693f5a651f876d3f546d26ddb
Eugene Bondarenko <eugenebondarenko@icloud.com>
2023-07-15 15:55:43 +0200

    code formatting for consistant look

--- hw-11-task1.ts
+++ hw-11-task1.ts
@@ -1,37 +1,48 @@
 const baseUrl = "https://jsonplaceholder.typicode.com/posts";
 
-async function getPost(num:number) {
-    const response = await fetch(baseUrl);
-    const json = await response.json();
-    console.log(json[num].title);
-    return json;
+async function getPost(num: number) {
+  const response = await fetch(baseUrl);
+  const json = await response.json();
+  console.log(json[num].title);
+  return json;
 }
-
 
 const showPosts = async () => {
-    await getPost(15)
-    .catch(() => {console.log(new Error("Error"))})
-    await getPost(23)
-    .catch(() => {console.log(new Error("Error"))})
-    await getPost(7)
-    .catch(() => {console.log(new Error("Error"))})
-    await getPost(3)
-    .catch(() => {console.log(new Error("Error"))})
-}
+  await getPost(15).catch(() => {
+    console.log(new Error("Error"));
+  });
+  await getPost(23).catch(() => {
+    console.log(new Error("Error"));
+  });
+  await getPost(7).catch(() => {
+    console.log(new Error("Error"));
+  });
+  await getPost(3).catch(() => {
+    console.log(new Error("Error"));
+  });
+};
 
 showPosts();
 
-//No await + .then() variant
+//Another variant with no await + .then()
 
 const showPostRow = () => {
-    getPost(13)
-    .catch(() => {console.log(new Error("Error"))})
+  getPost(13)
+    .catch(() => {
+      console.log(new Error("Error"));
+    })
     .then(() => getPost(20))
-    .catch(() => {console.log(new Error("Error"))})
+    .catch(() => {
+      console.log(new Error("Error"));
+    })
     .then(() => getPost(5))
-    .catch(() => {console.log(new Error("Error"))})
+    .catch(() => {
+      console.log(new Error("Error"));
+    })
     .then(() => getPost(1))
-    .catch(() => {console.log(new Error("Error"))})
-}
+    .catch(() => {
+      console.log(new Error("Error"));
+    });
+};
 
 showPostRow();
