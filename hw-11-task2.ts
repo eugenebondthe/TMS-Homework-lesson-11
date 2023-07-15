8c9516e2a9b573cddc31ce14f1dcba558cd78baa
Eugene Bondarenko <eugenebondarenko@icloud.com>
2023-07-15 16:00:56 +0200

    code formatting for consistant look

--- hw-11-task2.ts
+++ hw-11-task2.ts
@@ -1,23 +1,21 @@
 const basicURL = "https://jsonplaceholder.typicode.com/todos";
 
 async function getTodos() {
-    const response = await fetch(basicURL);
-    return await response.json();
+  const response = await fetch(basicURL);
+  return await response.json();
 }
 
 async function printTodos() {
-    let arr:object[] = [];
-    await getTodos().then(
-        (res) => {
-            for (let i = 0; i < res.length; i++) {
-                arr.push({
-                    id: res[i].id,
-                    title: res[i].title
-                })
-            }
-        }
-    )       
-    return arr;
+  let arr: object[] = [];
+  await getTodos().then((res) => {
+    for (let i = 0; i < res.length; i++) {
+      arr.push({
+        id: res[i].id,
+        title: res[i].title,
+      });
+    }
+  });
+  return arr;
 }
 
 console.log(printTodos().then((arr) => console.log(arr)));
