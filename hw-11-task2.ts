c2d1c3e7b7a3a69405be5459bc715f5e6fe32ef3
Eugene Bondarenko <eugenebondarenko@icloud.com>
2023-07-15 15:47:50 +0200

    second task implementation

---
+++ hw-11-task2.ts
@@ -0,0 +1,23 @@
+const basicURL = "https://jsonplaceholder.typicode.com/todos";
+
+async function getTodos() {
+    const response = await fetch(basicURL);
+    return await response.json();
+}
+
+async function printTodos() {
+    let arr:object[] = [];
+    await getTodos().then(
+        (res) => {
+            for (let i = 0; i < res.length; i++) {
+                arr.push({
+                    id: res[i].id,
+                    title: res[i].title
+                })
+            }
+        }
+    )       
+    return arr;
+}
+
+console.log(printTodos().then((arr) => console.log(arr)));
